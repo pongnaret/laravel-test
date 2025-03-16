c63e9e49336d783654718cae652cd01e68a920a0
pongnaret <147232179+pongnaret@users.noreply.github.com>
2025-03-16 22:18:47 +0700

    680316.2218

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    $title = 'About';
+    return view('about', compact('title'));
+});
+
+Route::get('/contact', function () {
+    $name = 'Tavon';
+    $age = 20;
+    $salary = 1000;
+
+    return view('contact', compact('name', 'age', 'salary'));
+});
+
+Route::get('/profile', function () {
+    return view('profile', ['name' => 'Tavon', 'age' => 20]);
+});
