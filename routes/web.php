6be80cc338014915cd270c000766801d05e7d82c
pongnaret <147232179+pongnaret@users.noreply.github.com>
2025-03-18 21:37:53 +0700

    680318.2137

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
@@ -25,4 +26,33 @@ Route::get('/profile', function () {
 
 Route::get('/params/{name}/{age}/{salary}', function ($name, $age, $salary) {
     return view('params', compact('name', 'age', 'salary'));
+});
+
+Route::get('/post', function () {
+    return view('post');
+});
+
+Route::post('/post', function (Request $request) {
+    $name = $request->name;
+    return json_encode(['name' => $name]);
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+//Http Response
+Route::get('/response', function () {
+    return response()->json(['name' => 'Tavon']);
+
+
+});
+
+Route::get('/products', function () {
+    $products = [
+        ['id' => 1, 'name' => 'Product 1'],
+        ['id' => 2, 'name' => 'Product 2'],
+        ['id' => 3, 'name' => 'Product 3'],
+    ];
+    return response()->json(['products' => $products]);
 });
